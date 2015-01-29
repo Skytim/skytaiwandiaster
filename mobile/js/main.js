d8fdd0b69074b7c90bab438c9f06cfd40656934f
Skytim <sky22357168@gmail.com>
2015-01-30 00:59:08 +0800

    按鈕測試2

--- mobile/js/main.js
+++ mobile/js/main.js
@@ -1,20 +1,33 @@
 $(document).ready(function () {
 
-var map = $('#map'),
-    m = {};
+var map = $('#map');
+
 
 map.tinyMap();
 
 // taipei button
 $('#taipei').on('click', function () {
-    alert("taipei");
+   
  $('#map').tinyMap('modify',{
+  
     kml: 'http://fema1.swcb.gov.tw/google/QueryRefugeMap.ashx'
 });
           
           
 });
+    
+
+// taipei button
+$('#water').on('click', function () {
+   alert("water");
+ $('#map').tinyMap('modify',{
+  
+    kml: 'http://fema1.swcb.gov.tw/google/Debris2011.kmz'
+});
           
+          
+});
+
           
 
 });
