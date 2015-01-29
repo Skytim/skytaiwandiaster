dd36349364c3811e49947f21efd306c046fed21c
Skytim <sky22357168@gmail.com>
2015-01-29 22:29:14 +0800

    按鈕程式初步完成

--- mobile/js/main.js
+++ mobile/js/main.js
@@ -1,8 +1,21 @@
 $(document).ready(function () {
-  $('#map').tinyMap();
-  $( "#taipei" ).click(function() {
-  alert( "taipei" );
-  });
+
+var map = $('#map'),
+    m = {};
+
+map.tinyMap();
+
+// taipei button
+$('#taipei').on('click', function () {
+    alert("taipei");
+ $('#map').tinyMap('modify',{
+    kml: 'http://fema1.swcb.gov.tw/google/QueryRefugeMap.ashx'
+});
+          
+          
+});
+          
+          
 
 });
 
