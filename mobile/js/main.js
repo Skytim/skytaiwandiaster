5923e7cbfc21920ffd3676bd88d71a0a2cc98f30
Skytim <sky22357168@gmail.com>
2015-02-01 19:25:22 +0800

    新增縣市

--- mobile/js/main.js
+++ mobile/js/main.js
@@ -2,27 +2,36 @@ $(document).ready(function () {
 
 var map = $('#map');
 
+// 模擬目前位置
+var current = ['25.04151536540612', '121.56354904174805'];
 
 map.tinyMap();
 
+
 // yilan button
 $('#yilan').on('click', function () {
- $('#map').tinyMap('panto', '宜蘭縣');
+
+ $('#map').tinyMap('panto', 'yilan ');
  $('#map').tinyMap('modify',{
     zoom:11,
     kml: 'http://skytaiwandb.azurewebsites.net/mobile/kmz/yilan.kmz'
 });
-          
-          
+      
 });
-    
+    $(".abc").click(function(){
+    $(this).attr("id")
 
-// taipei button
-$('#water').on('click', function () {
-   alert("water");
- $('#map').tinyMap('modify',{
+    }
+
+      )
+
+// keelung button
+$('#keelung').on('click', function () {
   
-    kml: 'http://fema1.swcb.gov.tw/google/Debris2011.kmz'
+ $('#map').tinyMap('panto', 'keelung');
+ $('#map').tinyMap('modify',{
+    zoom:11,
+    kml: 'http://skytaiwandb.azurewebsites.net/mobile/kmz/keelung.kmz'
 });
           
           
