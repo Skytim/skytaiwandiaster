399cfd4b252069826c4ecc4884225ea916069ffd
Skytim <sky22357168@gmail.com>
2015-02-11 01:04:36 +0800

    image_satilite

--- mobile/js/satellite_images.js
+++ mobile/js/satellite_images.js
@@ -1,34 +1,31 @@
+$(document).on("pageinit","#satellite_images",function(){
 
-$(document).on("mobileinit", function () {     
-	$.mobile.autoInitializePage = false;     
-	$.mobile.hashListeningEnabled = false;   
-});   
-function mobileInitPage() {    
-$.mobile.hashListeningEnabled = true;     
-$.mobile.initializePage();   
+   // 此处是 jQuery 事件...
    $("#east_asia_blue_cloud").click(function(){
-   	
-   		$("#bg").attr('src',"img/picture1.jpg");
+   		
+   		$('#cloud_image').attr('src','http://fema.swcb.gov.tw/wap/Handler/GetSatImage.ashx?imagetype=0');
    		return false;
    });
     $("#east_asia_infrared_imagery").click(function(){
-   		alert("east");
-   		$("#bg").attr('src',"img/picture1.jpg");
+   	
+   		$("#cloud_image").attr('src',"http://fema.swcb.gov.tw/wap/Handler/GetSatImage.ashx?imagetype=1");
    		return false;
    });
      $("#taiwan_blue_cloud").click(function(){
-   		alert("east");
-   		$("#bg").attr('src',"img/picture1.jpg");
+   		
+   		$("#cloud_image").attr('src',"http://fema.swcb.gov.tw/wap/Handler/GetSatImage.ashx?imagetype=2");
    		return false;
    });
       $("#taiwan_infrared_imagery").click(function(){
-   		alert("east");
-   		$("#bg").attr('src',"img/picture1.jpg");
+   		
+   		$("#cloud_image").attr('src',"http://fema.swcb.gov.tw/wap/Handler/GetSatImage.ashx?imagetype=3");
    		return false;
    });
       $("#radar_echo_image").click(function(){
-   		alert("east");
-   		$("#bg").attr('src',"img/picture1.jpg");
+   		
+   		$("#cloud_image").attr('src',"http://fema.swcb.gov.tw/wap/Handler/GetSatImage.ashx?imagetype=4");
    		return false;
    });
-}
+
+});
+
