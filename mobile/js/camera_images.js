91150cea3f606a9a55b8216cf63a008ab2e0cccf
Skytim <sky22357168@gmail.com>
2015-02-12 16:11:12 +0800

    上傳檔案

---
+++ mobile/js/camera_images.js
@@ -0,0 +1,15 @@
+$(document).on("pageinit","#camera_images",function(){
+   // 此处是 jQuery 事件...
+
+   $("#east_asia_blue_cloud").click(function(){
+   		$('#camera_image').attr('src','http://dfm.swcb.gov.tw/DebrisFinal/ShowCCDImg-FB.asp?StationID=7&CCDId=2');
+   		$( '#left-menu' ).panel( "close" );
+   });
+   $("#east_asia_infrared_imagery").click(function(){ 	
+   		$("#camera_image").attr('src',"http://dfm.swcb.gov.tw/DebrisFinal/ShowCCDImg-FB.asp?StationID=7&CCDId=3");
+   		$( '#left-menu' ).panel( "close" );
+         return false;
+   });
+
+});
+
