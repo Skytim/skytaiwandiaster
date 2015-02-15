0bf0d1690fbeb3a0c5d51702e077d3abb0db47a5
Skytim <sky22357168@gmail.com>
2015-02-15 17:01:43 +0800

    php first

--- mobile/allow.php
+++ mobile/allow.php
@@ -1,6 +1,6 @@
   <?php
  // Cross-Origin Resource Sharing Header
-  header('Access-Control-Allow-Origin: http://skytaiwandb.azurewebsites.net');
+  header('Access-Control-Allow-Origin: http://opendata.cwb.gov.tw');
   header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
   header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept'); 
   ?>
