38939564b563aea250738b5ac80987d3d0c4209a
Skytim <sky22357168@gmail.com>
2015-02-15 22:32:19 +0800

    php test

--- mobile/allow.php
+++ mobile/allow.php
@@ -1,4 +1,3 @@
 <?php
-$dataURL = "http://opendata.cwb.gov.tw/opendata/MFC/F-C0032-013.xml";
-readfile($dataURL);  // 調用PHP readfile()函數讀取遠端檔並返回
+readfile($_GET['url']);  // 調用PHP readfile()函數讀取遠端檔並返回
 ?>
