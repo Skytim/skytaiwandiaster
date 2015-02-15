71e45797bb3e16aaf4cc3ee3b9ed4d7b76f79f40
Skytim <sky22357168@gmail.com>
2015-02-15 16:07:52 +0800

    xmmp

--- mobile/weather.php
+++ mobile/weather.php
@@ -1,4 +1,11 @@
-<?php header('Access-Control-Allow-Origin: *'); ?>
+<?php
+ 
+// Cross-Origin Resource Sharing Header
+header('Access-Control-Allow-Origin: http://skytaiwandb.azurewebsites.net/');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept');
+ 
+?>
 <!DOCTYPE html>
 <html>
 <head>
