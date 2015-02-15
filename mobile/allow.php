d0f0f34d7caf5d979b00a68787e4e1aba59a039a
Skytim <sky22357168@gmail.com>
2015-02-15 17:18:08 +0800

    喔

--- mobile/allow.php
+++ mobile/allow.php
@@ -1,3 +1,3 @@
 //header("Access-Control-Allow-Origin: *"); //允許所有網域都可存取
-header("Access-Control-Allow-Origin: http://aa.example.com"); //僅允許http://aa.example.com網域存取
+header("Access-Control-Allow-Origin: *"); //僅允許http://aa.example.com網域存取
 echo 'test';
